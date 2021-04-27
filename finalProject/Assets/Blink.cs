d8b0ce2958be9e86e2ff879942fcbea174f1e2fd
cuong.nm172989@sis.hust.edu.vn <cuong.nm172989@sis.hust.edu.vn>
2021-04-28 00:38:02 +0700

    add Blink and UI

---
+++ finalProject/Assets/Blink.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam;
+
+    public Transform target;
+
+    private bool onUseTp = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.E)){
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.point);
+                }
+                if (hit.point.y <= 0.5)
+                {
+                    target.position = new Vector3(hit.point.x, target.position.y, hit.point.z);
+
+                }
+            }
+        }
+        
+    }
+}
---
+++ finalProject/Assets/CreateBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreateBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam;
+
+    private bool onUseTp = false;
+
+    public GameObject spawnee;
+
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.point);
+                    Instantiate(spawnee, transform.position, transform.rotation);
+                }
+
+            }
+        }
+    }
+}
